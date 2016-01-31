debf2e3972eff44a4914059e05ffdbed6947400c
brandon37 <brandonedu9@hotmail.com>
2016-01-30 23:23:54 -0800

    new view hydralab

--- application/controllers/hydralab.php
+++ application/controllers/hydralab.php
@@ -1,3 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Hydralab extends CI_Controller {
+class Hydralab extends CI_Controller {	
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	function index(){
+		$this->load->view('hydralab/bienvenido');
+	}
+
+}
+?>
