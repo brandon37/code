eb53f1c2a200438524a7e3a5f921e1082936566f
brandon37 <brandonedu9@hotmail.com>
2016-01-30 21:38:55 -0800

    Firt Commit

---
+++ application/controllers/hydralab.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Hydralab extends CI_Controller {
---
+++ application/views/welcome_message.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Curso de CodeIgniter</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1><?=$string?> mundo</h1>
+
+	
+</div>
+
+</body>
+</html>
