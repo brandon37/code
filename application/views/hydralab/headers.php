be57f0beb4d6cdad69ffd02688c03dbd49b7b279
brandon37 <brandonedu9@hotmail.com>
2016-02-01 10:59:04 -0800

    multiple views

---
+++ application/views/hydralab/headers.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Bienvenido a hydralab</title>
+	<link rel="stylesheet" href="">
+</head>
