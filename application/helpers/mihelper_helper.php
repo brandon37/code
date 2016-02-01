749d1f1f5cb92532bf49a0f746937796a0687033
brandon37 <brandonedu9@hotmail.com>
2016-02-01 11:19:24 -0800

    first helper

---
+++ application/helpers/mihelper_helper.php
@@ -0,0 +1,5 @@
+<?php
+	function getNombre(){
+		return "<h1>brandon rosales</1>";
+	}
+?>
--- application/views/hydralab/bienvenido.php
+++ application/views/hydralab/bienvenido.php
@@ -1,4 +1,5 @@
 <body>
 		<h1>Llamado desde el controlador hydralab</h1>
+		<?= getNombre();  ?>
 </body>
 </html>
